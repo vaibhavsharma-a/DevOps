5a4e600ec43607d19559cbb19ffacbfa2bf7a7a0
Vaibhav <vaibhav1863sharma@gmail.com>
2024-09-13 20:14:26 +0530

    first commit

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/main-page", handleMainPage)
+	http.HandleFunc("/health", handleHealth)
+
+	addr := "localhost:8080"
+	log.Printf("The server is running on the port %s...", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleMainPage(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	returnResponse(writer, "This is the main page")
+}
+
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	returnResponse(writer, "All Good!")
+}
+
+func returnResponse(w http.ResponseWriter, responseString string) {
+	response := []byte(responseString)
+	_, err := w.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
